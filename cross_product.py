bff51f81fdf057e4f8598337fe3437d85189901c
Hiranyagarbha <88590577+H1ranyagarbha@users.noreply.github.com>
2023-04-20 21:21:11 +0530

    Create cross_product.py

---
+++ cross_product.py
@@ -0,0 +1,29 @@
+import numpy as n
+
+def cross_product():
+  print()
+  print("The cross product of Vector A and Vector B is equal to this vector:")
+
+  c_product_1=[]
+  for i in range(0,3):
+     l=int(input("List out the coordinates of the first vector:"))
+     c_product_1.append(l)
+  print("Vector A's Components are:")
+  print(c_product_1)
+
+  c_product_2=[]
+  for i in range(0,3)
+     k=int(input("List out the coordinates of the second vector:"))
+     s_product_2.append(k)
+  print("Vector B's components are:")
+  print(c_product_2)
+
+  vectorAbar = np.array(c_product_1)
+  vectorBbar = np.array(c_product_2)
+
+  print("The cross product of Vector A and Vector B is equal to:")
+
+  print(); print(np.cross(vectorAbar, vectorBbar))
+
+
+cross_product()
